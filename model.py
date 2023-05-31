07bea8d83b1f79426e7c90dc10cd3395e0b33152
Ghani-25 <54942476+Ghani-25@users.noreply.github.com>
2023-05-31 14:57:54 +0200

    Update model.py

--- model.py
+++ model.py
@@ -2,12 +2,13 @@ import pinecone
 from sentence_transformers import SentenceTransformer, util
 
 model = SentenceTransformer("Ghani-25/LF_enrich_sim", device='cpu')
-pinecone.init(api_key='55f76c4b-d5e7-43f8-9ea4-60209aad21b4', environment='northamerica-northeast1-gcp')
-index = pinecone.Index('ai-find')
+pinecone.init(api_key='6da2a094-a957-4f4d-8892-059610a05980', environment='northamerica-northeast1-gcp')
+index = pinecone.Index('find-region')
 
 def enrichir(query, count):
     xq = model.encode(query).tolist()
     result = index.query(xq, top_k=count, includeMetadata=False)
     res = result.to_dict() #conversion to dict
     lis = list(res.values())[0]
-    return lis
+    filtered_lis = [{'id': i['id'], 'score': i['score']} for i in lis if i['score'] > 0.75]
+    return filtered_lis
